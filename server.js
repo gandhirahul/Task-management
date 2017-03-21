5146eb6e7e4193ddd125baf51900a6bdf7353b1a
Rahul Gandhi <rahulgandhi38@gmail.com>
2017-03-22 00:40:51 +0530

    adding server file

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var app     = express();
+var path    = require("path");
+
+app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/bower_components'));
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/dist/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+app.listen(3000);
+
+console.log("Running at Port 3000");
